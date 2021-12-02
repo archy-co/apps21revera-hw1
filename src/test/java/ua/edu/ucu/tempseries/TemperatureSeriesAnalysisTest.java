6496cacc2c6dfb307066472143ca3e12a588ec2e
Archy <archy.revera@protonmail.com>
2021-12-02 16:53:47 +0200

    Add more tests

--- src/test/java/ua/edu/ucu/tempseries/TemperatureSeriesAnalysisTest.java
+++ src/test/java/ua/edu/ucu/tempseries/TemperatureSeriesAnalysisTest.java
@@ -28,6 +28,16 @@ public class TemperatureSeriesAnalysisTest {
         seriesAnalysis.average();
     }
 
+    @Test(expected = IllegalArgumentException.class)
+    public void testEmptyArray() {
+        double[] temperatureSeries = {};
+        TemperatureSeriesAnalysis seriesAnalysis = new TemperatureSeriesAnalysis(temperatureSeries);
+
+        seriesAnalysis.max();
+        seriesAnalysis.min();
+
+    }
+
     @Test
     public void testAverage() {
         double[] temperatureSeries = {3.0, -5.0, 1.0, 5.0};
@@ -38,6 +48,82 @@ public class TemperatureSeriesAnalysisTest {
         
         assertEquals(expResult, actualResult, 0.00001);        
     }
-    
 
+    @Test
+    public void testMin() {
+        double[] temperatureSeries = {3.0, -5.0, 1.0, 5.0};
+        TemperatureSeriesAnalysis seriesAnalysis = new TemperatureSeriesAnalysis(temperatureSeries);
+
+        assertEquals(-5.0, seriesAnalysis.min(), 0.00001);
+
+        double[] temperatureSeries2 = {3.25};
+        TemperatureSeriesAnalysis seriesAnalysis2 = new TemperatureSeriesAnalysis(temperatureSeries2);
+
+        assertEquals(3.25, seriesAnalysis2.min(), 0.00001);
+    }
+
+    @Test
+    public void testMax() {
+        double[] temperatureSeries = {3.0, -5.0, 1.0, 5.0};
+        TemperatureSeriesAnalysis seriesAnalysis = new TemperatureSeriesAnalysis(temperatureSeries);
+
+        assertEquals(5.0, seriesAnalysis.max(), 0.00001);
+    }
+
+    @Test
+    public void testClosestToZero() {
+        double[] temperatureSeries = {3.0, -5.0, 1.0, 5.0};
+        TemperatureSeriesAnalysis seriesAnalysis = new TemperatureSeriesAnalysis(temperatureSeries);
+
+        assertEquals(1.0, seriesAnalysis.findTempClosestToZero(), 0.00001);
+    }
+
+    @Test
+    public void testDeviation() {
+        double[] temperatureSeries = {3.0, 3.0, 3.0, 3.0};
+        TemperatureSeriesAnalysis seriesAnalysis = new TemperatureSeriesAnalysis(temperatureSeries);
+
+        assertEquals(0, seriesAnalysis.deviation(), 0.00001);
+
+        double[] temperatureSeries2 = {-3, 2, 3, 1, 4};
+        TemperatureSeriesAnalysis seriesAnalysis2 = new TemperatureSeriesAnalysis(temperatureSeries2);
+
+        assertEquals(2.701, seriesAnalysis2.deviation(), 0.001);
+    }
+
+    @Test
+    public void testFindTempsLessThen() {
+        double[] temperatureSeries = {4.63, 14.12, 11.72, -1.04, -5.89};
+        TemperatureSeriesAnalysis seriesAnalysis = new TemperatureSeriesAnalysis(temperatureSeries);
+
+        double[] expectedRes1 = {-1.04, -5.89};
+
+        assertTrue(expectedRes1.length == seriesAnalysis.findTempsLessThen(0).length);
+        assertTrue(expectedRes1[0] == seriesAnalysis.findTempsLessThen(0)[0]);
+        assertTrue(expectedRes1[1] == seriesAnalysis.findTempsLessThen(0)[1]);
+    }
+
+    @Test
+    public void findTempsGreaterThen() {
+        double[] temperatureSeries = {4.63, 14.12, 11.72, -1.04, -5.89};
+        TemperatureSeriesAnalysis seriesAnalysis = new TemperatureSeriesAnalysis(temperatureSeries);
+
+        double[] expectedRes1 = {14.12, 11.72};
+
+        assertTrue(expectedRes1.length == seriesAnalysis.findTempsGreaterThen(11.72).length);
+        assertTrue(expectedRes1[0] == seriesAnalysis.findTempsGreaterThen(11.72)[0]);
+        assertTrue(expectedRes1[1] == seriesAnalysis.findTempsGreaterThen(11.72)[1]);
+    }
+
+    @Test
+    public void addTemps() {
+        double[] temperatureSeries = {4.63, 14.12, 11.72, -1.04, -5.89};
+        TemperatureSeriesAnalysis seriesAnalysis = new TemperatureSeriesAnalysis(temperatureSeries);
+
+        double[] additionals = {-3.24, 2.19, 3.43, 1.13, 4.7};
+
+        assertEquals(10, seriesAnalysis.addTemps(additionals));
+        assertEquals(4.63, seriesAnalysis.temperatureSeries[0], 0.01);
+        assertEquals(-3.24, seriesAnalysis.temperatureSeries[5], 0.01);
+    }
 }
